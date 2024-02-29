3b53ac364f62d7180d54091f852c3c2356681253
Christine <chriztine.cyrus@gmail.com>
2024-02-29 15:28:42 +1100

    changes in user-form and created validateForm() in script.js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,50 @@
 globalThis
 
+function genderInputCheck(maleChecked,femaleChecked){
+    // check if none or both radio buttons are entered
+    if ((maleChecked&&femaleChecked)||(!maleChecked&&!femaleChecked)){
+        return false;
+    }
+    
+    return true;
+
+}
 function replaceName(){
     let name = prompt("Siapa nama anda?","pengunjung");
     document.getElementById("visitor-name").innerHTML=name;
 }
 
+function validateForm(){
+    //check if any field is empty
+    //outputs the user's input
+    var name = document.forms["user-form"]["name"].value;
+    var dob = document.forms["user-form"]["dob"].value;
+    var message = document.forms["user-form"]["message"].value; 
+    var maleChecked = document.getElementById("male").checked;
+    var femaleChecked = document.getElementById("female").checked;
+    var gender = "";
+
+    if (genderInputCheck(maleChecked,femaleChecked)){
+        if (maleChecked){
+            gender = "Laki-Laki";
+        }else if (femaleChecked){
+            gender = "Perempuan";
+        }
+    }
+    else{
+        //warning to choose one
+    }
+
+    if ((name=="")||(dob=="")||(message=="")||(gender=="")){
+        // sned error message, return false
+        console.log("empty field(s)")      
+    }
+    else{
+        // putput all the info, return true
+    }
+
+}
+
 replaceName();
+
+document.getElementById("send-submit").addEventListener("click",()=>validateForm())
